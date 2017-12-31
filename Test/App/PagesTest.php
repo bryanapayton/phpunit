2558d6704ca0139b48391713788eb4bc9b15d9a5
root <root@ip-172-31-47-147.ec2.internal>
2017-12-31 23:58:22 +0000

    test 6

--- Test/App/PagesTest.php
+++ Test/App/PagesTest.php
@@ -12,4 +12,21 @@ $this->assertEquals($expected, $pages->render());
 
 }
 
+public function testReturnTrueReturnsTrue()
+{
+$pages = new Pages();
+$this->assertTrue($pages->returnTrue());
+}
+
+public function testReturnArrayReturnValidArray()
+{
+$pages = new Pages();
+$this->assertTrue(is_array($pages->returnArray()));
+}
+public function testReturnArrayReturnsNonEmptyArray()
+{
+$pages = new Pages();
+$this->assertTrue((count($pages->returnArray()) > 0));
+}
+
 }
